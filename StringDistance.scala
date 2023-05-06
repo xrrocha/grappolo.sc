43eef25e30a6f8b1541f1d8a0802e756fce7b74d
Ricardo Rocha <ricardo@xrrocha.net>
2023-05-06 11:53:28 -0500

    Added Levenshtein metric. Added ability to select metric by name

--- StringDistance.scala
+++ StringDistance.scala
@@ -1,9 +1,24 @@
-import info.debatty.java.stringsimilarity.Damerau
+import StringDistance.{computeDamerauDistance, computeLevenshteinDistance}
+import info.debatty.java.stringsimilarity.{Damerau, NormalizedLevenshtein}
+
 import scala.math.max
 
 object StringDistance:
-  lazy val damerau = Damerau()
-  def computeDamerauDistance: (String, String) => Double =
+
+  val computeDamerauDistance: (String, String) => Double =
+    val damerau = Damerau()
     (e1: String, e2: String) =>
       if e1 == e2 then 0.0
       else damerau.distance(e1, e2) / max(e1.length, e2.length).toDouble
+
+  val computeLevenshteinDistance: (String, String) => Double =
+    val levenshtein = NormalizedLevenshtein()
+    (e1: String, e2: String) =>
+      if e1 == e2 then 0.0
+      else levenshtein.distance(e1, e2)
+
+  private val distanceMetrics = Map(
+    "damerau" -> computeDamerauDistance,
+    "levenshtein" -> computeLevenshteinDistance
+  )
+  def apply(metricName: String) = distanceMetrics(metricName)
