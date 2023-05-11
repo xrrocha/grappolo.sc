6f79b24713fc94ca1a376c906de5b66e10a81760
Ricardo Rocha <ricardo@xrrocha.net>
2023-05-11 16:32:56 -0500

    Simplified clusterer by suppressing Matrix type

--- Grappolo.scala
+++ Grappolo.scala
@@ -1,12 +1,16 @@
+import scala.annotation.tailrec
+
 object Grappolo:
+
   def clustererFor[A](
       generatePairs: Iterable[A] => Iterable[(A, A)],
       distanceMetric: (A, A) => Double,
       maxDistance: Double
   ): Iterable[A] => Seq[Set[A]] =
-    // Return suitable clusterer for entries
-    entries =>
 
+    require(maxDistance > 0.0 && maxDistance <= 1.0)
+
+    entries =>
       val scores: Seq[(A, A, Double)] =
         generatePairs(entries)
           .filter((entry, neighbor) => entry != neighbor)
@@ -15,51 +19,23 @@ object Grappolo:
           .filter: (_, _, distance) =>
             distance > 0.0 && distance <= maxDistance
           .toSeq
-
-      val matrix: Matrix[A] =
-        Matrix(entries.size, scores): (entry, neighbor) =>
-          if entry == neighbor then 0.0
-          else distanceMetric(entry, neighbor)
-
-      val clusterMap: Map[A, Set[A]] =
-
-        val initialClusterMap: Map[A, Set[A]] =
-          entries
-            .map(entry => (entry, Set(entry)))
-            .toMap
-
-        val clusteringDistance = matrix.maxProfileDistance
-
-        scores
-          .filter((_, _, distance) => distance <= clusteringDistance)
-          .sortBy((_, _, distance) => distance)
-          .foldLeft(initialClusterMap): (runningClusterMap, score) =>
-            val (entry, neighbor, _) = score
-
-            runningClusterMap ++
-              matrix
-                .agglomerate(
-                  runningClusterMap(entry),
-                  runningClusterMap(neighbor)
-                )
-                .flatMap: cluster =>
-                  cluster.map(entry => (entry, cluster))
-      end clusterMap
-
-      clusterMap.values.toSeq.distinct
+      cluster(entries, scores, distanceMetric)
   end clustererFor
-end Grappolo
 
-object Matrix:
-  def apply[A](
-      size: Int,
-      scores: Iterable[(A, A, Double)]
-  )(
-      defaultMapping: (A, A) => Double
-  ): Matrix[A] =
+  def cluster[A](
+      entries: Iterable[A],
+      scores: Seq[(A, A, Double)],
+      distanceMetric: (A, A) => Double
+  ): Seq[Set[A]] =
+
+    def defaultMapping(entry: A, neighbor: A) =
+      if entry == neighbor then 0.0
+      else distanceMetric(entry, neighbor)
+
     val symmetricScores: Iterable[(A, A, Double)] =
       scores.map: (entry, neighbor, distance) =>
         (neighbor, entry, distance)
+
     val map =
       (scores ++ symmetricScores)
         .groupBy((entry, _, _) => entry)
@@ -71,9 +47,87 @@ object Matrix:
               .withDefault(neighbor => defaultMapping(entry, neighbor))
         .withDefault: entry =>
           Map().withDefault(neighbor => defaultMapping(entry, neighbor))
-    require(size >= map.size)
-    new Matrix(size, map)
-  end apply
+
+    val distances: Seq[Double] =
+      scores
+        .map((_, _, distance) => distance)
+        .distinct
+        .sorted
+
+    val maxProfileDistance: Double =
+      def neighborhoodProfiles(distanceThreshold: Double): Set[Set[A]] =
+        map.values.toSeq
+          .map: neighbors =>
+            neighbors.toSeq
+              .filter((neighbor, distance) => distance <= distanceThreshold)
+              .map((neighbor, distance) => neighbor)
+              .toSet
+          .toSet
+      end neighborhoodProfiles
+      // Choose distance yielding highest metric for neighborhood profile count
+      distances
+        .zip {
+          distances
+            .map(neighborhoodProfiles)
+            .map: neighborhoodProfile =>
+              val profileCount = neighborhoodProfile.size
+              // Normalize profile count w/respect to given size
+              (profileCount - 1) / (entries.size - 1).toDouble
+        }
+        .maxBy: (distance, profileCountScore) =>
+          val similarity = 1.0 - distance
+          similarity * profileCountScore
+        ._1
+    end maxProfileDistance
+
+    def clusterDistance(cluster1: Set[A], cluster2: Set[A]): Double =
+      val scores =
+        cluster1.flatMap: entry1 =>
+          cluster2.map(entry2 => map(entry1)(entry2))
+      scores.sum / scores.size
+    end clusterDistance
+
+    def agglomerate(cluster1: Set[A], cluster2: Set[A]): Seq[Set[A]] =
+      @tailrec
+      def go(clusters: Seq[Set[A]]): Seq[Set[A]] =
+        if clusters.size < 2 then clusters
+        else
+          val (i, j, distance) =
+            cartesianProduct(clusters.indices)
+              .map: (i, j) =>
+                (i, j, clusterDistance(clusters(i), clusters(j)))
+              .toSeq
+              .minBy: (i, j, distance) =>
+                (distance, clusters(i).size + clusters(j).size)
+          if distance > maxProfileDistance then clusters
+          else
+            go:
+              (clusters(i) ++ clusters(j)) +:
+                clusters.indices
+                  .filter(c => c != i && c != j)
+                  .map(clusters)
+      go(IndexedSeq(cluster1, cluster2))
+    end agglomerate
+
+    val clusterMap: Map[A, Set[A]] =
+      val initialClusterMap: Map[A, Set[A]] =
+        entries
+          .map(entry => (entry, Set(entry)))
+          .toMap
+      scores
+        .filter((_, _, distance) => distance <= maxProfileDistance)
+        .sortBy((_, _, distance) => distance)
+        .foldLeft(initialClusterMap): (runningClusterMap, score) =>
+          val (entry, neighbor, _) = score
+
+          runningClusterMap ++
+            agglomerate(runningClusterMap(entry), runningClusterMap(neighbor))
+              .flatMap: cluster =>
+                cluster.map(entry => (entry, cluster))
+    end clusterMap
+
+    clusterMap.values.toSeq.distinct
+  end cluster
 
   def cartesianProduct[A](entries: Iterable[A]): Iterable[(A, A)] =
     val seq = entries.toIndexedSeq
@@ -81,74 +135,4 @@ object Matrix:
       .from(seq.indices)
       .flatMap(i => (i + 1 until seq.size).map(j => (i, j)))
       .map((i, j) => (seq(i), seq(j)))
-end Matrix
-
-import scala.annotation.tailrec
-class Matrix[A](size: Int, map: Map[A, Map[A, Double]]):
-  def distances: Seq[Double] =
-    map.values
-      .flatMap: neighbors =>
-        neighbors.map((neighbor, distance) => distance)
-      .toSeq
-      .distinct
-      .sorted
-
-  lazy val maxProfileDistance: Double =
-    def neighborhoodProfiles(distanceThreshold: Double): Set[Set[A]] =
-      map.values.toSeq
-        .map: neighbors =>
-          neighbors.toSeq
-            .filter((neighbor, distance) => distance <= distanceThreshold)
-            .map((neighbor, distance) => neighbor)
-            .toSet
-        .toSet
-    end neighborhoodProfiles
-    // Choose distance yielding highest metric for neighborhood profile count
-    distances
-      .zip {
-        distances
-          .map(neighborhoodProfiles)
-          .map: neighborhoodProfile =>
-            val profileCount = neighborhoodProfile.size
-            // Normalize profile count w/respect to given size
-            (profileCount - 1) / (size - 1).toDouble
-      }
-      .maxBy: (distance, profileCountScore) =>
-        val similarity = 1.0 - distance
-        similarity * profileCountScore
-      ._1
-  end maxProfileDistance
-
-  def agglomerate(
-      cluster1: Set[A],
-      cluster2: Set[A]
-  ): Seq[Set[A]] =
-    @tailrec
-    def go(clusters: Seq[Set[A]]): Seq[Set[A]] =
-      if clusters.size < 2 then clusters
-      else
-        val (i, j, distance) =
-          Matrix
-            .cartesianProduct(clusters.indices)
-            .map: (i, j) =>
-              (i, j, clusterDistance(clusters(i), clusters(j)))
-            .toSeq
-            .minBy: (i, j, distance) =>
-              (distance, clusters(i).size + clusters(j).size)
-        if distance > maxProfileDistance then clusters
-        else
-          go:
-            (clusters(i) ++ clusters(j)) +:
-              clusters.indices
-                .filter(c => c != i && c != j)
-                .map(clusters)
-    go(IndexedSeq(cluster1, cluster2))
-  end agglomerate
-
-  def clusterDistance(cluster1: Set[A], cluster2: Set[A]): Double =
-    val scores =
-      cluster1.flatMap: entry1 =>
-        cluster2.map(entry2 => map(entry1)(entry2))
-    scores.sum / scores.size
-  end clusterDistance
-end Matrix
+end Grappolo
