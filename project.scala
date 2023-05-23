e5a93219388860b7d8dd70b500bdf1ea45626a31
Ricardo Rocha <ricardo@xrrocha.net>
2023-05-22 20:28:43 -0500

    Updated to latest scala-cli syntax

--- project.scala
+++ project.scala
@@ -1,9 +1,11 @@
-//> using javaOpt "-Xmx4g"
-//> using scala "3.3.0-RC6"
-//> using options "-deprecation", "-release:11"
-//> using resourceDirs "src/main/resources", "src/test/resources"
+//> using javaOpt -Xmx4g
+//> using scala 3.3.0-RC6
+//> using options -deprecation -release:11
+//> using resourceDirs src/main/resources src/test/resources
 
-//> using dependency "info.debatty:java-string-similarity:2.0.0"
-//> using dependency "org.scala-lang.modules::scala-parallel-collections::1.0.4"
+// //> using options "-coverage-out:${.}"
 
-//> using dependency "org.scalameta::munit::1.0.0-M7"
+//> using dep info.debatty:java-string-similarity:2.0.0
+//> using dep org.scala-lang.modules::scala-parallel-collections::1.0.4
+
+//> using test.dep org.scalameta::munit::1.0.0-M7
